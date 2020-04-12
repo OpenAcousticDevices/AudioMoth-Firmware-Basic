0f7daa62e4ce1608640e17c49e32f163e853ab00
Bertrik Sikken <bertrik@sikken.nl>
2020-04-13 01:15:24 +0200

    Simplify composition of WAV comment.

--- main.c
+++ main.c
@@ -150,37 +150,28 @@ void setHeaderComment(uint32_t currentTime, int8_t timezoneHours, int8_t timezon
 
     /* Format comment field */
     char *comment = wavHeader.icmt.comment;
-    sprintf(comment, "Recorded at %02d:%02d:%02d %02d/%02d/%04d (UTC", time->tm_hour, time->tm_min, time->tm_sec, time->tm_mday, 1 + time->tm_mon, 1900 + time->tm_year);
-    comment += 36;
-    if (timezoneHours < 0) sprintf(comment, "%d", timezoneHours);
-    if (timezoneHours > 0) sprintf(comment, "+%d", timezoneHours);
-    if (timezoneHours < 0 || timezoneHours > 0) comment += 2;
-    if (timezoneHours < -9 || timezoneHours > 9) comment += 1;
-    if (timezoneMinutes < 0) sprintf(comment, ":%2d", -timezoneMinutes);
-    if (timezoneMinutes > 0) sprintf(comment, ":%2d", timezoneMinutes);
-    if (timezoneMinutes < 0 || timezoneMinutes > 0) comment += 3;
-    sprintf(comment, ") by %s at gain setting %d while battery state was ", artist, (unsigned int)gain);
-    comment += 74;
+    comment += sprintf(comment, "Recorded at %02d:%02d:%02d %02d/%02d/%04d (UTC", time->tm_hour, time->tm_min, time->tm_sec, time->tm_mday, 1 + time->tm_mon, 1900 + time->tm_year);
+    if (timezoneHours < 0) comment += sprintf(comment, "%d", timezoneHours);
+    if (timezoneHours > 0) comment += sprintf(comment, "+%d", timezoneHours);
+    if (timezoneMinutes < 0) comment += sprintf(comment, ":%2d", -timezoneMinutes);
+    if (timezoneMinutes > 0) comment += sprintf(comment, ":%2d", timezoneMinutes);
+    comment += sprintf(comment, ") by %s at gain setting %d while battery state was ", artist, (unsigned int)gain);
 
     if (batteryState == AM_BATTERY_LOW) {
-        sprintf(comment, "less than 3.6V.");
-        comment += 15;
+        comment += sprintf(comment, "less than 3.6V.");
     } else if (batteryState >= AM_BATTERY_FULL) {
-        sprintf(comment, "greater than 4.9V.");
-        comment += 18;
+        comment += sprintf(comment, "greater than 4.9V.");
     } else {
         batteryState += 35;
-        sprintf(comment, "%01d.%01dV.", batteryState / 10, batteryState % 10);
-        comment += 5;
+        comment += sprintf(comment, "%01d.%01dV.", batteryState / 10, batteryState % 10);
     }
 
     if (batteryVoltageLow || switchPositionChanged) {
-        sprintf(comment, " Recording cancelled before completion due to ");
-        comment += 46;
+        comment += sprintf(comment, " Recording cancelled before completion due to ");
         if (batteryVoltageLow) {
-            sprintf(comment, "low battery voltage.");
+            comment += sprintf(comment, "low battery voltage.");
         } else if (switchPositionChanged) {
-            sprintf(comment, "change of switch position.");
+            comment += sprintf(comment, "change of switch position.");
         }
     }
 }
